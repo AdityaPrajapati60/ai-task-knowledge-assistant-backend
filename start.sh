aac0949edd71e7e8a2912f9c2251646d7c8b8a6b
AdityaPrajapati60 <ap17082004@gmail.com>
2026-02-06 12:46:06 +0530

    fix CRLF in start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-#!/usr/bin/env bash
-echo "Starting FastAPI app..."
-echo "PORT is: ${PORT}"
-
-exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT}
+#!/usr/bin/env bashecho "Starting FastAPI app..."echo "PORT is: ${PORT}"exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT}
