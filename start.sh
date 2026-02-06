97277ab593e9a71aec80db6d55a8dc6d74ef24b5
AdityaPrajapati60 <ap17082004@gmail.com>
2026-02-06 10:56:44 +0530

    fix render startup script

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-10000}
+set -e
+
+echo "Starting FastAPI app..."
+echo "PORT is: ${PORT}"
+
+exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT}
+
