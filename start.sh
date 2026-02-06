f02859ae29305b5596cfc6419db3f9df2eb09dfa
AdityaPrajapati60 <ap17082004@gmail.com>
2026-02-06 12:48:13 +0530

    fix start.sh formatting

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-#!/usr/bin/env bashecho "Starting FastAPI app..."echo "PORT is: ${PORT}"exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT}
+#!/usr/bin/env bash
+echo "Starting FastAPI app..."
+echo "PORT is: ${PORT}"
+
+exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT}
